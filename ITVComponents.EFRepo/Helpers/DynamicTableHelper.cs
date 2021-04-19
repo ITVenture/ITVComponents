9795e44bcf880aaabf8af7704b5b401c9b9c6ac2
Matthias Wyler <mw@it-venture.ch>
2021-04-19 15:29:34 +0200

    Extension in DynamicTableHelper. Optional IgnoreCase (default: true)

--- ITVComponents.EFRepo/Helpers/DynamicTableHelper.cs
+++ ITVComponents.EFRepo/Helpers/DynamicTableHelper.cs
@@ -9,16 +9,16 @@ namespace ITVComponents.EFRepo.Helpers
 {
     public static class DynamicTableHelper
     {
-        public static TableDiff[] CompareDefinitions(ICollection<TableColumnDefinition> table1Definition, ICollection<TableColumnDefinition> table2Definition)
+        public static TableDiff[] CompareDefinitions(ICollection<TableColumnDefinition> table1Definition, ICollection<TableColumnDefinition> table2Definition, bool ignoreCase = true)
         {
-            var allColumns = table1Definition.Select(n => n.ColumnName).Union(table2Definition.Select(n => n.ColumnName)).Distinct().ToArray();
+            var allColumns = table1Definition.Select(n => n.ColumnName).Union(table2Definition.Select(n => ignoreCase?n.ColumnName.ToLower():n.ColumnName)).Distinct().ToArray();
             var tmp = (from a in allColumns
-                join s in table1Definition on a equals s.ColumnName into sg
+                join s in table1Definition on a equals ignoreCase ? s.ColumnName.ToLower() : s.ColumnName into sg
                 from sc in sg.DefaultIfEmpty()
-                join d in table2Definition on a equals d.ColumnName into dg
+                join d in table2Definition on a equals ignoreCase ? d.ColumnName.ToLower() : d.ColumnName into dg
                 from dc in dg.DefaultIfEmpty()
-                orderby sc?.Position??dc.Position
-                select new  TableDiff{ColumnName = a, Table1Def= sc, Table2Def= dc}).ToArray();
+                orderby sc?.Position ?? dc.Position
+                select new TableDiff {ColumnName = a, Table1Def = sc, Table2Def = dc}).ToArray();
             return tmp;
         }
     }
