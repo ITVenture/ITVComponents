26774b24e8e34d975ee6ad21953f5ecaf942b6a6
wylermat <mw@it-venture.ch>
2025-08-19 11:56:27 +0200

    some blazor stuff

---
+++ ITVComponents.WebCoreToolkit.Blazor/Configuration/StubComponentConfiguration.cs
@@ -0,0 +1,30 @@
+﻿namespace ITVComponents.WebCoreToolkit.Blazor.Configuration
+{
+    public class StubComponentConfiguration
+    {
+        private Dictionary<string, Type> stubRegisters = new Dictionary<string, Type>();
+
+        public void RegisterStubComponent<T>(string name) where T : class
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Name cannot be null or whitespace.", nameof(name));
+            }
+            if (stubRegisters.ContainsKey(name))
+            {
+                throw new InvalidOperationException($"A stub component with the name '{name}' is already registered.");
+            }
+            stubRegisters[name] = typeof(T);
+        }
+
+        public Type? GetStubComponentType(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Name cannot be null or whitespace.", nameof(name));
+            }
+            stubRegisters.TryGetValue(name, out var type);
+            return type;
+        }
+    }
+}
---
+++ ITVComponents.WebCoreToolkit.Blazor/Extensions/DependencyExtensions.cs
@@ -0,0 +1,13 @@
+﻿using ITVComponents.WebCoreToolkit.Blazor.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ITVComponents.WebCoreToolkit.Blazor.Extensions
+{
+    public static class DependencyExtensions
+    {
+        public static IServiceCollection ConfigureStubComponents(this IServiceCollection services, Action<StubComponentConfiguration> configure)
+        {
+            return services.Configure(configure);
+        }
+    }
+}
