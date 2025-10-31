2192b69c770dd6c44ba633c6aaadaab708743770
wylermat <mw@it-venture.ch>
2025-10-31 11:11:38 +0100

    improved input-dialog handling for custom mode

--- ITVComponents.WebCoreToolkit.Net.TelerikUi/lib/js/Tools/Dialog.js
+++ ITVComponents.WebCoreToolkit.Net.TelerikUi/lib/js/Tools/Dialog.js
@@ -368,7 +368,7 @@
             visible: false,
             title: ITVenture.Text.getText("Popup_Input_Title", "Input"),
             minWidth: 400,
-            minHeight: 140,
+            minHeight: 190,
             modal: true,
             scrollable: false,
             /*position: {
@@ -394,8 +394,12 @@
         });
 
         obj.onShow(function (window, dialog, refObj) {
-            var inputArea = '<input id="UserInput" class="flex-fill k-textbox" />';
+            var inputArea = '<input id="UserInput" class="flex-fill k-input k-textbox" />';
             var buttonArea = obj.defaultButtons;
+            var hasDefault = false;
+            var defaultValue = "";
+            var defaultSize = { width: 400, height: 190 };
+            dialog.setOptions(defaultSize);
             obj.mode = "default";
             if (typeof refObj === "string") {
                 obj.window.children("[id='AlertMessage']").html(refObj);
@@ -404,10 +408,14 @@
                 if (typeof refObj.Controls !== "undefined") {
                     inputArea = refObj.Controls;
                     obj.mode = "custom";
-                } else {
-                    obj.window.find("[id='UserInput']").val(refObj.Default);
+                } else if (typeof refObj.Default !== "undefined") {
+                    defaultValue = refObj.Default;
+                    hasDefault = true;
                 }
 
+                if (typeof refObj.Size === "object") {
+                    dialog.setOptions({ width: typeof refObj.Size.width !== "undefined" ? refObj.Size.width : defaultSize.width, height: typeof refObj.Size.height !== "undefined" ? refObj.Size.height : defaultSize.height });
+                }
                 if (typeof refObj.Buttons !== "undefined") {
                     obj.mode = obj.mode.concat("WithButtons");
                     buttonArea = "";
@@ -430,6 +438,9 @@
             }
             obj.window.children("[tag='cip-Content']").html(inputArea);
             obj.window.children("[tag='buttons']").html(buttonArea);
+            if (hasDefault) {
+                obj.window.find("[id='UserInput']").val(defaultValue);
+            }
             //obj.window.children("[id='AlertMessage']").html(refObj);
         });
 
