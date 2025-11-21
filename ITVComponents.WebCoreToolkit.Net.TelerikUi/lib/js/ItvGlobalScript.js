084dbbd23d0f81e8a33aa9e2cd190ebd8413ea49
wylermat <mw@it-venture.ch>
2025-11-21 15:55:22 +0100

    FrontendSecurityFix

--- ITVComponents.WebCoreToolkit.Net.TelerikUi/lib/js/ItvGlobalScript.js
+++ ITVComponents.WebCoreToolkit.Net.TelerikUi/lib/js/ItvGlobalScript.js
@@ -354,7 +354,10 @@ window.ITVenture = {
                 ITVenture.FrontendSecurity.featuresUsed = true;
                 var tmp = await ITVenture.Ajax.ajaxGet("~/TenantFeatures");
                 for (var i = 0; i < tmp.length; i++) {
-                    ITVenture.FrontendSecurity.activeFeatures.push(tmp[i]);
+                    var item = tmp[i];
+                    if (item.Enabled) {
+                        ITVenture.FrontendSecurity.activeFeatures.push(tmp[i]);
+                    }
                 }
             };
             $(document).ready(function () { ITVenture.FrontendSecurity.featureDownload = asyncFx(); });
@@ -384,7 +387,7 @@ window.ITVenture = {
             await ITVenture.FrontendSecurity.featureDownload;
             var lowerFeat = featureName.toLowerCase();
             var retVal = !ITVenture.FrontendSecurity.featuresUsed
-                || ITVenture.FrontendSecurity.activeFeatures.findIndex(n => n.FeatureName.toLowerCase() == lowerFeat) != -1;
+                || ITVenture.FrontendSecurity.activeFeatures.findIndex(n => n.FeatureName.toLowerCase() == lowerFeat && n.Enabled) != -1;
             return retVal;
         }
     }
