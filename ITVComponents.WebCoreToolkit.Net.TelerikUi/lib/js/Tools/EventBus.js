e4e0a2e0d97184cad7e241279e6274519058fc83
wylermat <mw@it-venture.ch>
2025-11-25 10:11:36 +0100

    small fixes and extensions
    - small extension for the eventBus-class
     - Contains a SubClass MiniAwaitable which is basically a thenable-implementation
    - fix on the event-bus class
     - when an event is dispatched with no eventArguments, the event is still correctly fired.
    - fix in FrontendSecurity
     - improved control-flow for download/presentation of permissions and features

--- ITVComponents.WebCoreToolkit.Net.TelerikUi/lib/js/Tools/EventBus.js
+++ ITVComponents.WebCoreToolkit.Net.TelerikUi/lib/js/Tools/EventBus.js
@@ -23,7 +23,7 @@
         };
 
         ITVenture.Tools.EventBus.Reactor.prototype.dispatchEvent = function (eventName, eventArgs) {
-            var ignoreCancel = !eventArgs.hasOwnProperty("defaultPrevented");
+            var ignoreCancel = typeof (eventArgs) === "undefined" || eventArgs == null || !eventArgs.hasOwnProperty("defaultPrevented");
             var retVal = true;
             this.events[eventName].callbacks.forEach(function (callback) {
                 if (ignoreCancel || !eventArgs.defaultPrevented) {
@@ -44,6 +44,47 @@
         ITVenture.Tools.EventBus.EventArgs.prototype.preventDefault = function () {
             this.defaultPrevented = true;
         }
+
+        ITVenture.Tools.EventBus.MiniAwaitable.prototype.then = function (success, fail) {
+            if (!this.completed) {
+                if (typeof (success) === "function") {
+                    this.callbacks.addEventListener("completed", success);
+                }
+
+                if (typeof (fail) === "function") {
+                    this.callbacks.addEventListener("error", fail);
+                }
+            }
+            else {
+                if (!this.failed) {
+                    success(this.result);
+                }
+                else {
+                    fail(this.result);
+                }
+            }
+        }
+
+        ITVenture.Tools.EventBus.MiniAwaitable.prototype.resolve = function (result) {
+            this.completed = true;
+            this.result = result;
+            this.callbacks.dispatchEvent("completed", result);
+        }
+
+        ITVenture.Tools.EventBus.MiniAwaitable.prototype.reject = function (error) {
+            this.completed = true;
+            this.failed = true;
+            this.result = error;
+            this.callbacks.dispatchEvent("error", error);
+        }
+    },
+    MiniAwaitable: function () {
+        this.callbacks = new ITVenture.Tools.EventBus.Reactor();
+        this.callbacks.registerEvent("completed");
+        this.callbacks.registerEvent("error");
+        this.completed = false;
+        this.failed = false;
+        this.result = null;
     }
 };
 
